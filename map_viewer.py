4d854c3bb1640b57f542712c629bf07ea0a6dbcb
Saiyam-Sandhir <saiyamsandhir@gmail.com>
2023-01-30 15:08:08 +0530

    Add map widget to the MapFrame tk.Frame object
    resolve #3

--- map_viewer.py
+++ map_viewer.py
@@ -3,4 +3,7 @@ import tkintermapview as tkmap
 
 class MapFrame(tk.Frame):
     def __init__(self, master, COL_THEME):
-        super().__init__(master, height = 350, bg = "red")
+        super().__init__(master, height = 350, bg = COL_THEME["bg_col"])
+
+        self.map_widget = tkmap.TkinterMapView(self, corner_radius = 10)
+        self.map_widget.place(relx = 0.5, rely = 0.5, relheight = 0.9, relwidth = 0.8, anchor = tk.CENTER)
