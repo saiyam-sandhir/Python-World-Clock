fca40ff000cd377811ec9cb3c9cf6951f45af8fc
Saiyam-Sandhir <saiyamsandhir@gmail.com>
2023-01-30 09:45:00 +0530

    Give struncture and color theme to the app window
    Create two frames-
    - map_frame: To put the map viewer and related widgets
    - clocks_frame: To put the clocks

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import tkinter as tk
+import configparser
 
 class WorldClockApp(tk.Tk):
     def __init__(self):
         super().__init__()
+
+        settings = configparser.ConfigParser()
+        settings.read("settings.ini")
+
+        COL_THEME = dict(settings["col_theme"])
         
         self.title("Python World Clock")
         self.iconbitmap(".\\images\\icon.ico")
         self.geometry("700x700")
+        self["background"] = COL_THEME["bg_col"]
+
+        map_frame = tk.Frame(self, height = 350, bg = COL_THEME["bg_col"])
+        map_frame.pack(fill = tk.BOTH, expand = 1, pady = 0)
+
+        clocks_frame = tk.Frame(self, height = 350, bg = COL_THEME["bg_col"])
+        clocks_frame.pack(fill = tk.BOTH, expand = 1)
 
         self.mainloop()
 
