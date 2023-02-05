a72f1e388196b141ea6519ca0405f0cbe976972b
Saiyam-Sandhir <saiyamsandhir@gmail.com>
2023-02-05 13:43:13 +0530

    Add locate button on the timebar
    resolve #13

--- clock.py
+++ clock.py
@@ -37,8 +37,11 @@ class TimeBar(ctk.CTkFrame):
         date_Label = tk.Label(self, text = datetime.now(pytz.timezone(self.timezone)).strftime("%d/%m/%Y"), bg = COL_THEME["fg_col"], fg = COL_THEME["txt_col"], font = ("Arial", 10))
         date_Label.place(relx = 0.52, y = 35, anchor = tk.CENTER)
 
-        #will only be viable for TimeBar.place(relx = 0.5, rely = 0.5, width = 600, anchor = tk.CENTER) in WorldClockApp
-        deleteimg_image = ctk.CTkImage(light_image = Image.open(".\\images\\delete.png"), dark_image = Image.open("C:\\Users\\pc\\Desktop\\Python-World-Clock\\images\\delete.png"), size = (20, 20))
+        locatorimg_image = ctk.CTkImage(light_image = Image.open(".\\images\\locator.png"), dark_image = Image.open(".\\images\\locator.png"), size = (20, 20))
+        locator_Button = ctk.CTkButton(self, image = locatorimg_image, text = "", corner_radius = 10, height = 20, width = 20, command = lambda: [map_widget.set_position(self.details["lat"], self.details["long"]), map_widget.set_zoom(15)])
+        locator_Button.place(x = 540, y = 25, anchor = tk.E)
+
+        deleteimg_image = ctk.CTkImage(light_image = Image.open(".\\images\\delete.png"), dark_image = Image.open(".\\images\\delete.png"), size = (20, 20))
         delete_Button = ctk.CTkButton(self, image = deleteimg_image, text = "", corner_radius = 10, height = 20, width = 20, command = lambda: master.updateClocksFrame(list(timebar_details.values()), COL_THEME, map_widget))
         delete_Button.place(x = 590, y = 25, anchor = tk.E)
 
